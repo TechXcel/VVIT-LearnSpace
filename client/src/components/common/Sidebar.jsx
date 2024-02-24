4888b9f08ae243489a7ffc1f720d6478e0c96286
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2024-02-25 00:23:28 +0530

    update: dashboard icon

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -4,12 +4,25 @@ import Faculty from "../icons/User";
 import { Button } from "../ui/button";
 import { ScrollArea } from "../ui/scroll-area";
 import { sidebarItems } from "@/data/sidebarItems";
+import Dashboard from "../icons/Dashboard";
 
 const Sidebar = () => {
   return (
     <ScrollArea>
       <div className="flex flex-col p-2 gap-y-3 md:p-6">
         <div className="space-y-4">
+          <div className="px-3 py-3">
+            <Link to="/admin">
+              <Button
+                variant="ghost"
+                className="justify-start w-full text-lg font-semibold tracking-tight"
+              >
+                <Dashboard />
+                Dashboard
+              </Button>
+            </Link>
+          </div>
+
           <div className="px-3 py-3">
             <h2 className="px-4 mb-2 text-lg font-semibold tracking-tight">
               Users
